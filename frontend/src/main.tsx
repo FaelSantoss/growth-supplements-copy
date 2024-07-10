b14e5e485e26419c2514dd9be8c0769cf388ff0c
FaelSantoss <silva.faelm@gmail.com>
2024-07-10 14:16:53 -0300

    config tailwind

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import ProductsList from './components/ProductsList.tsx'
+import './styles/output.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <ProductsList />
   </React.StrictMode>,
 )
