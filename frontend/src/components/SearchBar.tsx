1c601f872a7b06a3746691af156e41ab82c292e9
FaelSantoss <silva.faelm@gmail.com>
2024-07-11 10:06:42 -0300

    created search bar component

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,19 @@
+// src/components/SearchBar.tsx
+import React from 'react';
+interface SearchBarProps {
+    placeholder?: string;
+  }
+
+const SearchBar: React.FC<SearchBarProps> = ({ placeholder = 'Pesquisar...'}) => {
+  return (
+    <div className="flex justify-center items-center mx-8 w-full max-w-xl">
+      <input 
+        type="text" 
+        placeholder={placeholder}
+        className="w-full max-w-xl p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
