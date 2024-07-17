c4db7360d47537e57ef515d50638b2bfbc29c858
FaelSantoss <silva.faelm@gmail.com>
2024-07-17 09:39:35 -0300

    migration for passwordReset

---
+++ prisma/migrations/20240714192725_create_password_reset_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "PasswordReset" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE INDEX "email_index" ON "PasswordReset"("email");
