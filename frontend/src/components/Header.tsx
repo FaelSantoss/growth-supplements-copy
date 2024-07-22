3bd57196592410d43038ff2df4d3ab049ccfd3c6
FaelSantoss <silva.faelm@gmail.com>
2024-07-22 11:55:17 -0300

    add cart modal in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useAuth } from '../context/AuthContext';
+import CartModal from '../components/CartModal';
 import SearchBar from "./SearchBar";
 import ProductsList from "./ProductsList";
 
@@ -10,22 +11,12 @@ interface Category {
 
 const Header: React.FC = () => {
   const [categories, setCategories] = useState<Category[]>([]);
-  const [showModal, setShowModal] = useState(false);
+  const [activeModal, setActiveModal] = useState<string | null>(null);
   const { isAuthenticated, userLogged, logout } = useAuth();
 
-  let timeoutId: NodeJS.Timeout;
-
-  const openModal = () => {
-    clearTimeout(timeoutId);
-    setShowModal(true);
-  };
-
-  const closeModal = () => {
-    timeoutId = setTimeout(() => {
-      setShowModal(false);
-    }, 200);
-  };
-
+  const openModal = (modalName: string) => setActiveModal(modalName);
+  const closeModal = () => setActiveModal(null);
+  
   useEffect(() => {
     fetch('http://localhost:3001/category/')
       .then(response => response.json())
@@ -39,70 +30,56 @@ const Header: React.FC = () => {
         <a href="/"><img src="/logo.png" alt="logo" /></a>
         <SearchBar placeholder="Encontre o suplemento ideal para você" />
         <img className="w-8 h-8 mr-3" src="/do-utilizador.png" alt="usuario" />
-        { isAuthenticated ? (
+        {isAuthenticated ? (
           <>
             <p className="text-white">Olá, <a href="#"><strong>{userLogged?.name}</strong></a></p>
             <button 
-            onClick={logout} 
-            className="mx-12 bg-red-500 text-white font-bold rounded focus:outline-none focus:shadow-outline"
-          >
-            Sair
-          </button>
+              onClick={logout} 
+              className="mx-12 bg-red-500 text-white font-bold rounded focus:outline-none focus:shadow-outline"
+            >
+              Sair
+            </button>
           </>
-        ): (
-        <div>
-          <p className="text-white">
-            <a href="/register"><strong>Cadastre-se</strong></a> ou
-          </p>
-          <a className="text-white" href="/login"><strong>faça seu login</strong></a>
-        </div>
+        ) : (
+          <div>
+            <p className="text-white">
+              <a href="/register"><strong>Cadastre-se</strong></a> ou
+            </p>
+            <a className="text-white" href="/login"><strong>faça seu login</strong></a>
+          </div>
         )}
-        <img className="w-8 h-8 ml-80" src="/carrinho.png" alt="carrinho" />
+        <a onClick={() => openModal('cart')} className="cursor-pointer"><img className="w-8 h-8 ml-80" src="/carrinho.png" alt="carrinho" /></a>
       </header>
+
       <header
-        className="bg-white shadow-lg-inner flex items-center justify-between p-3 rounded-lg relative"
+        className="bg-white shadow-lg-inner flex items-center justify-between p-3 relative"
         style={{ boxShadow: 'inset 0 0 20px rgba(0, 0, 0, 0.1)' }}
       >
-        {!showModal && (
-          <div className="mx-4">
-            <a
-              href="#"
-              className="text-black hover:text-blue_primary flex"
-              onMouseEnter={openModal}
-              onMouseLeave={closeModal}
-            >
-              <img className="w-6 h-6 mr-2" src="/menu.png" alt="logo" />
-              TODAS AS CATEGORIAS
-            </a>
-          </div>
-        )}
-        {showModal && (
-          <div className="mx-4">
-            <a
-              href="#"
-              className="text-blue_primary flex"
-              onMouseEnter={openModal}
-              onMouseLeave={closeModal}
-            >
-              <img className="w-6 h-6 mr-2" src="/menu2.png" alt="logo" />
-              TODAS AS CATEGORIAS
-            </a>
-          </div>
-        )}
+        <div className="mx-4">
+          <a
+            href="#"
+            className={`text-${activeModal === 'categories' ? 'blue_primary' : 'black'} hover:text-blue_primary flex`}
+            onMouseEnter={() => openModal('categories')}
+            onMouseLeave={closeModal}
+          >
+            <img className="w-6 h-6 mr-2" src={`/menu${activeModal === 'categories' ? '2' : ''}.png`} alt="logo" />
+            TODAS AS CATEGORIAS
+          </a>
+        </div>
         <div>
           {categories.map(category => (
-            <a className="mx-2 text-gray_200  hover:text-blue_primary" href="#" key={category.id}>
+            <a className="mx-2 text-gray_200 hover:text-blue_primary" href="#" key={category.id}>
               {category.name}
             </a>
           ))}
         </div>
       </header>
       <div
-        onMouseEnter={openModal}
+        onMouseEnter={() => openModal('categories')}
         onMouseLeave={closeModal}
         className="relative"
       >
-        {showModal && (
+        {activeModal === 'categories' && (
           <div 
             className="bg-white p-4 shadow-lg absolute w-11/12 left-1/2 transform -translate-x-1/2 z-50 flex flex-wrap">
             {categories.map(category => (
@@ -114,6 +91,9 @@ const Header: React.FC = () => {
           </div>
         )}
       </div>
+      {activeModal === 'cart' && (
+        <CartModal isVisible={true} onClose={closeModal} />
+      )}
     </>
   );
 }
